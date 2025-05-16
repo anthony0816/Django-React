6014a399871793d283a7ae1bcd2c551928bbbce0
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-05-16 16:04:29 -0400

    Asignando ID del usuario a en sesion a cada tarea

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default= 1)
     title = models.CharField( max_length=50)
     description = models.TextField(blank= True)
     done = models.BooleanField(default=False)
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,31 @@
 import { createTask } from "../api/task.api"
 import styles from "./TaskFormPage.module.css";
+import { jwtDecode } from "jwt-decode";
 
 export function TaskFormPage() {
+    
+    function BuildTask(event) {
+    
+    event.preventDefault(); // Evita la recarga del formulario
+    let title = document.getElementById("title_input").value;
+    let description = document.getElementById("description_input").value;
+    let done = document.getElementById("checkbox_input").checked;
+    const token = localStorage.getItem('access_token')
+    const token_decodificado = jwtDecode(token)
+    const user_id = token_decodificado.user_id
+    
+    const task = {
+        user_id: user_id,
+        title: title,        
+        description: description, 
+        done: done        
+    };
+    console.log("Enviando tarea:", task);
+    createTask(task);
+        
+}
+
+    
     return (
         <div className={styles.container}>
                 <form onSubmit={BuildTask} className={styles.form}>
@@ -20,20 +44,5 @@ export function TaskFormPage() {
     );
 }
 
-function BuildTask(event) {
-    event.preventDefault(); // Evita la recarga del formulario
-    let title = document.getElementById("title_input").value;
-    let description = document.getElementById("description_input").value;
-    let done = document.getElementById("checkbox_input").checked;
-    
-    const task = {
-        title: title,        
-        description: description, 
-        done: done        
-    };
-    console.log("Enviando tarea:", task);
-    createTask(task);
-        
-}
 
 
