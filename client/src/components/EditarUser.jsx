a7832b1901a80a6ad215caf450df57a6b05fab26
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-05-19 12:02:18 -0400

    recuperando los datos del formilario Editar

--- client/src/components/EditarUser.jsx
+++ client/src/components/EditarUser.jsx
@@ -2,14 +2,26 @@ import "./EditarUser.css"
 
 export function EditarUser({ user , onClose} ) {
     
+    function HandleSubmit(e){
+        e.preventDefault();
+        const user_id = user.id
+        const username = document.querySelector(`[name="username${user.id}"]`);
+        const email = document.querySelector(`[name="email${user.id}"]`);
+        const is_staff = document.querySelector(`[name="is_staff${user.id}"]`);
+        const is_superuser = document.querySelector(`[name="is_superuser${user.id}"]`);
+        console.log("user",user_id, username.value,email.value,is_staff.checked,is_superuser.checked)
+        const User = {
+
+        }
+    }
     
     return (
-        <form className="user-form">
+        <form className="user-form" onSubmit={HandleSubmit}>
             <div className="form-row">
                 <label className="form-label">Username:</label>
                 <input 
                     type="text" 
-                    name="username" 
+                    name= {`username${user.id}`} 
                     defaultValue={user.username} 
                     className="form-input"
                 />
@@ -19,7 +31,7 @@ export function EditarUser({ user , onClose} ) {
                 <label className="form-label">Email:</label>
                 <input 
                     type="email" 
-                    name="email" 
+                    name={`email${user.id}`} 
                     defaultValue={user.email} 
                     className="form-input"
                 />
@@ -29,7 +41,7 @@ export function EditarUser({ user , onClose} ) {
                 <label className="form-checkbox-label">
                     <input 
                         type="checkbox" 
-                        name="is_staff" 
+                        name={`is_staff${user.id}`} 
                         defaultChecked={user.is_staff} 
                         className="form-checkbox"
                     />
@@ -41,7 +53,7 @@ export function EditarUser({ user , onClose} ) {
                 <label className="form-checkbox-label">
                     <input 
                         type="checkbox" 
-                        name="is_superuser" 
+                        name={`is_superuser${user.id}`} 
                         defaultChecked={user.is_superuser} 
                         className="form-checkbox"
                     />
